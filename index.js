09686347671faeb00268a6dd0a3bd0a83032d786
lucas2298 <tienthang2298@gmail.com>
2019-09-16 14:06:24 +0700

    update

---
+++ index.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let socket = require('socket.io');
+
+// App setup
+let app = express();
+let server = app.listen(process.env.PORT || 4000, function() {
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup
+let io = socket(server);
+
+io.on('connection', function(socket) {
+    console.log(socket.id);
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
+});
