e12e30b5d0fdc22ac65b18df11f86e053dfcf1ee
lucas2298 <tienthang2298@gmail.com>
2019-10-14 12:48:23 +0700

    khong co gi

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 https://www.youtube.com/watch?v=KNqVpESuyQo
 https://www.youtube.com/watch?v=UzNqdD8EiAo&list=PL8ClR1xrtR4sCTLCFFFc3PKLHr6hPnXea&index=5
 https://cdnjs.com/libraries/socket.io
+http://www.nettruyen.com/truyen-tranh/hoa-hong-nien-dai-hac-cot-duong/chap-1/342728
--- data/csvtojson.py
+++ data/csvtojson.py
@@ -1,16 +1,28 @@
 import csv, json
 
 csvFilePath = 'test.csv'
+jsonFilePath = 'test.json'
 
-with open('test.csv', "r", encoding='utf-8-sig') as csvFile:
+data = {}
+
+with open(csvFilePath, "r", encoding='utf-8-sig') as csvFile:
     csvReader = csv.DictReader(csvFile)
+    data['intents'] = []
+    # for rows in csvReader:
+    #     oldstr = rows['selectList']
+    #     oldstr = oldstr.replace("'", "")
+    #     newstr = oldstr[1:len(oldstr)-1]
+    #     print(newstr.split(','))
+
     for rows in csvReader:
         print(rows['tag'])
+        s = ""
+        for i in rows:
+            # data['intents'].append({
+            #     i: rows[i],
+            # })
+            s += i + ': ' + rows[i] + ',' + '\n'
+        print(s,'\n')
 
-with open('test.csv', "r", encoding='utf-8-sig') as csvFile:
-    csvReader = csv.DictReader(csvFile)
-    for rows in csvReader:
-        oldstr = rows['selectList']
-        oldstr = oldstr.replace("'", "")
-        newstr = oldstr[1:len(oldstr)-1]
-        print(newstr.split(','))
+    with open(jsonFilePath, 'w', encoding='utf-8-sig') as json_data:
+        json.dump(data, json_data)
